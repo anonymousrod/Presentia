db7c3df76fcfa72f7e4c6702239db9ee744d3512
teteganexauce <teteganexauce@gmail.com>
2026-05-15 10:50:27 +0100

    Initialisation du projet & intégration du template

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,13 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard.index');
-})->name('dashboard');
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
 Route::post('/logout', function () {
     Auth::logout();
